3550fb347257443b64990e6943f97a1bd0cfc7b0
ViktorYeet <viktor@stefansbo.se>
2024-09-25 15:27:50 +0200

    BSTree is complete and working as intended. me thinks

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,2 +1,65 @@
-public class BinarySearchTree {
+public class BinarySearchTree<T extends Comparable<T>> implements DataStructure<T> {
+    private Node<T> root;
+
+    private class Node<T extends Comparable<T>> {
+        private T data;
+        private Node<T> left;
+        private Node<T> right;
+
+        public Node(T data) {
+            this.data = data;
+        }
+
+        void addChild(Node<T> child) {
+            int cmp = child.data.compareTo(this.data);
+            if (cmp == 0) return;
+            else if(cmp < 0) {
+                if (left != null) {
+                    left.addChild(child);
+                    return;
+                }
+                this.left = child;
+            }
+            else if (cmp > 0) {
+                if (right != null) {
+                    right.addChild(child);
+                    return;
+                }
+                this.right = child;
+            }
+        }
+
+        public String toString(String indent) {
+            String s = new String();
+            s += data.toString();
+            if(left != null) s += "\n" + indent +left.toString(indent + "-");
+            if(right != null) s += "\n" + indent + right.toString(indent + "-");
+            return s;
+        }
+
+        void printRecursiveNode(String indent) {
+            System.out.println(indent + this.data.toString());
+            if(left != null) left.printRecursiveNode(indent+ "l.");
+            if(right != null) right.printRecursiveNode(indent + "r.");
+        }
+
+        
+    }
+    public BinarySearchTree() {}
+    public void pushItem(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (root == null) {
+            root = newNode;
+        }
+        else root.addChild(newNode);
+    }
+
+    @Override
+    public String toString() {
+        return root.toString("-");
+    }
+
+    public void printTree() {
+        root.printRecursiveNode("");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        BinaryTree<Integer> mybintree= new BinaryTree<Integer>();
-        Integer[] values = new Integer[20];
+        BinarySearchTree<Integer> mybintree = new BinarySearchTree<Integer>();
+        Integer[] values = new Integer[1000];
+        Random rand = new Random();
+        for (int i = 0; i < values.length; i++) {
+            values[i] = rand.nextInt(values.length);
+        }
 
         for(Integer value : values) {
             mybintree.pushItem(value);
         }
-        System.out.println(mybintree);
+        mybintree.printTree();
     }
 }
