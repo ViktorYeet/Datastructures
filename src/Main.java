62df3e911d24f270172bb63d0b9a031b5c41d62f
ViktorYeet <viktor@stefansbo.se>
2024-10-25 22:26:43 +0200

    cooler main function

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,14 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args) {
-        TwoThreeTree<Integer, String> mybintree = new TwoThreeTree<Integer, String>();
+        TwoThreeTree<String, Integer> mybintree = new TwoThreeTree<>();
         String[] testdata = {"Hello", "world", ",", "what", "a", "wonderful", "world", "it", "is!"}; // 9 things
         //Collections.shuffle(Arrays.asList(testdata));
 
         for(int i = 0; i < testdata.length; i++) {
-            mybintree.pushItem(i, testdata[i]);
+            mybintree.pushItem(testdata[i], i);
         }
 
-        System.out.println(mybintree.getItem(5));
-        System.out.println(mybintree.toString());
+        System.out.println(mybintree.getItem("wonderful"));
+        System.out.println(mybintree);
     }
 }
